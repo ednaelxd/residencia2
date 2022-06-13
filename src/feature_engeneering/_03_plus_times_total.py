bd3e5f326c39cd0ebc245e9e6a128b07bf808f99
Ednael Francisco Vieira da Silva <efvs@cin.ufpe.br>
2022-06-13 02:46:16 -0300

    att 13.06

--- src/feature_engeneering/_03_plus_times_total.py
+++ src/feature_engeneering/_03_plus_times_total.py
@@ -2,37 +2,54 @@ import numpy as np
 
 def calcula_pct_teto(data):
     
-    # Inicialização
+    #Inicialização
+    #data2 = data.copy()
+    #data2['pct'] = 0
     data = np.array(data)
-    pct_teto = []
+    lista_zerada = [0]
+    pct_teto=[]
+    #pct_teto = lista_zerada*data.shape[0]
     ###############
-    for linhas in data:
-        
-        if linhas[-1] == 0:
-            #print(linhas)
+    cont=0
+
+    for linhas in data:     
+
+        #pct_teto=0
+        if linhas[-2] == 0:
             #TAG'AMENTO DAS LICITACOES DDO TIPO: REGISTRO DE PRECO
-            if (linhas[4]=='Pregão - Registro de Preço' or linhas[4]=='Concorrência - Registro de Preço' or linhas[4]=='Concorrência') and (linhas[-2]=='Obras'):
-                pct_teto.append(linhas[-3] / 3300000)
-
-            if (linhas[4]=='Pregão - Registro de Preço') and (linhas[-2]=='Compras/Servicos'):
-                pct_teto.append(linhas[-3] / 1430000)
-
+            if ((linhas[4]=='Pregão - Registro de Preço' or linhas[4]=='Concorrência - Registro de Preço' or linhas[4]=='Concorrência') and (linhas[-3]=='Obra')):
+                pct_teto.append(linhas[-4] / 3300000)
+                #print(cont, "entrou aq")
+            elif ((linhas[4]=='Pregão - Registro de Preço') and (linhas[-3]=='Compras/Servicos')):
+                pct_teto.append(linhas[-4] / 1430000)
+                #print(cont, "entrou aq")
             #TAG'AMENTO DAS LICITACOES DO TIPO: DISPENSA LICITACAO
-            if (linhas[4]=='Dispensa de Licitação') and (linhas[-2]=='Obras'):
-                pct_teto.append(linhas[-3] / 33000)
-            if (linhas[4]=='Dispensa de Licitação')  and (linhas[-2]=='Compras/Servicos'):
-                pct_teto.append(linhas[-3] / 17600)
+            elif ((linhas[4]=='Dispensa de Licitação') and (linhas[-3]=='Obra')):
+                pct_teto.append(linhas[-4] / 33000)
+                ##print(cont, "entrou aq")
+
+            elif ((linhas[4]=='Dispensa de Licitação')  and (linhas[-3]=='Compras/Servicos')):
+                pct_teto.append(linhas[-4] / 17600)
+                #print(cont, "entrou aq")
 
             #TAG'AMENTO DAS LICITACOES DO TIPO: TOMADA DE PREÇOS
-            if (linhas[4]=='Tomada de Preços')  and (linhas[-2]=='Obras'):
-                pct_teto.append(linhas[-3] / 3300000)                          
-            if (linhas[4]=='Tomada de Preços')  and (linhas[-2]=='Compras/Servicos'):
-                pct_teto.append(linhas[-3] / 1400000)
+            elif ((linhas[4]=='Tomada de Preços')  and (linhas[-3]=='Obra')):
+                pct_teto.append(linhas[-4] / 3300000)
+                #print(cont, "entrou aq")
+
+            elif ((linhas[4]=='Tomada de Preços')  and (linhas[-3]=='Compras/Servicos')):
+                pct_teto.append(linhas[-4] / 1400000)
+                #print(cont, "entrou aq")
 
             #TAG'AMENTO DAS LICITACOES DO TIPO: CONVITE
-            if (linhas[4]=='Convite')  and (linhas[-2]=='Obras'):
-                pct_teto.append(linhas[-3] / 176000)
-            if (linhas[4]=='Convite')   and (linhas[-2]=='Compras/Servicos'):
-                pct_teto.append(linhas[-3] / 330000)
-        
+            elif ((linhas[4]=='Convite')  and (linhas[-3]=='Obra')):
+                pct_teto.append(linhas[-4] / 176000)
+                #print(cont, "entrou aq")
+
+            elif ((linhas[4]=='Convite')   and (linhas[-3]=='Compras/Servicos')):
+                pct_teto.append(linhas[-4] / 330000)
+                #print(cont, "entrou aq")
+                
+        #data2.iloc[cont,-1]=pct_teto
+        cont+=1
     return(pct_teto)
